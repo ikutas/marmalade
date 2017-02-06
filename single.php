b0c906765d4d0c6c69422e4a312f22bb7ccf5d3c
nabettu <tatsuaki.w@gmail.com>
2017-02-06 16:33:43 +0900

    single scss作成

--- single.php
+++ single.php
@@ -1,29 +1,31 @@
 <?php get_header(); ?>
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area single__content">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
-			<?php blank_post_nav(); ?>
+			<?php //TODO:配布時には出す
+				//blank_post_nav(); ?>
 
 			<?php
-				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
 		<?php endwhile;?>
 
-		<?php if(function_exists('wp_related_posts')): ?>
+		<?php //TODO: 配布時には消す
+			if(function_exists('wp_related_posts')): ?>
 			<div class="single__related--post">
 				<h3><i class="fa fa-edit"></i> こちらの記事もよく読まれています</h3>
 				<?php wp_related_posts()?>
 			</div>
 		<?php endif; ?>
 
-		<?php if(function_exists('wpp_get_mostpopular')): ?>
+		<?php //TODO: 配布時には消す
+			if(function_exists('wpp_get_mostpopular')): ?>
 			<div class="single__bottom_popular--post">
 				<h3><i class="fa fa-trophy"></i> 今日の人気記事</h3>
 				<div class="ninki-post" align="center">
